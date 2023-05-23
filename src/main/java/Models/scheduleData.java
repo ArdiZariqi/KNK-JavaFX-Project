b2b3e9b1fb6d394956938e97cc7d9de7fe779755
agnesajashanicaa <122025593+agnesajashanicaa@users.noreply.github.com>
2023-05-23 14:18:46 +0200

    commit14

--- src/main/java/Models/scheduleData.java
+++ src/main/java/Models/scheduleData.java
@@ -5,20 +5,20 @@ import java.sql.Date;
 
 public class scheduleData {
 
-    private Integer scheduleId;
+    private String scheduleId;
     private String scheduleDay;
     private String scheduleTime;
     private String scheduleCourse;
 
 
-    public scheduleData(Integer scheduleId, String scheduleDay, String scheduleTime, String scheduleCourse) {
+    public scheduleData(String scheduleId, String scheduleDay, String scheduleTime, String scheduleCourse) {
         this.scheduleId = scheduleId;
         this.scheduleDay = scheduleDay;
         this.scheduleTime = scheduleTime;
         this.scheduleCourse = scheduleCourse;
     }
 
-    public Integer getScheduleId() {
+    public String getScheduleId() {
         return scheduleId;
     }
 
